c2ba3931a1c151a17ac4c0b5c6edde7bb617fb50
Andres Guisado <andresguisado@users.noreply.github.com>
2020-07-01 20:53:27 +0100

    Update ansible-ssh-user-pwd.sh

--- ansible-ssh-user-pwd.sh
+++ ansible-ssh-user-pwd.sh
@@ -8,10 +8,10 @@ CONJUR_AUTHN_LOGIN=$CONJUR_AUTHN_LOGIN
 #echo "[INFO] Coping new conjur_variable.py"
 #sudo cp conjur_variable.py /usr/lib/python3.6/site-packages/ansible/plugins/lookup/conjur_variable.py
 echo "[INFO] Retrieving DAP public certificate"
-openssl s_client -showcerts -connect dap-master2.cyberarkdemo.net:443 < /dev/null 2> /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > conjur.pem
+openssl s_client -showcerts -connect dap-master < /dev/null 2> /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > conjur.pem
 
 echo "[INFO] Preparing DAP parameters for ansible"
-export CONJUR_APPLIANCE_URL="https://dap-master2.cyberarkdemo.net"
+export CONJUR_APPLIANCE_URL="https://dap-master"
 export CONJUR_ACCOUNT="cybr"
 export CONJUR_CERT_FILE="conjur.pem"
 export ANSIBLE_HOST_KEY_CHECKING="False"
